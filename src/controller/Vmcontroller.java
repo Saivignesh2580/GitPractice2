136a2da93d43988d52b9dde78ed35fd3346db037
vignesh <saivignesh8328@gmail.com>
2025-09-10 14:06:40 +0530

    adding getVas endpoint in vmController

--- src/controller/Vmcontroller.java
+++ src/controller/Vmcontroller.java
@@ -1,4 +1,8 @@
 @Controller
 public class Vmcontroller{
     
+    @GetMapping("/getvas")
+    public ResponseEntity<List<virtualAccpunt>> getVas(@AuthenticationPrincipal User user){
+        
+    }
 }
