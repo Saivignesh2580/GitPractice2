a639f2a2faae4f7d890e52dff26355c56147253c
vignesh <saivignesh8328@gmail.com>
2025-09-10 17:37:36 +0530

    creating VArepo and adding data

---
+++ src/model/VARepo.java
@@ -0,0 +1,10 @@
+package model;
+
+public class VARepo {
+    private int id;
+    private int category;
+    private int limitAmount;
+    private LocalDate limitDate;
+    private boolean isemergency;
+    private double balance;
+}
