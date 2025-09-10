3545b96d7217cb6d2e0cce76875632704a419da3
vignesh <saivignesh8328@gmail.com>
2025-09-10 16:55:27 +0530

    adding getters and setters to VADto

--- src/Dto/VirtualAccount.java
+++ src/Dto/VirtualAccount.java
@@ -8,4 +8,40 @@ public class VirtualAccount {
     private int limitAmount;
     private LocalDate limitDate;
     private boolean isemergency;
+    private double balance;
+    public int getId() {
+        return id;
+    }
+    public int getCategory() {
+        return category;
+    }
+    public int getLimitAmount() {
+        return limitAmount;
+    }
+    public LocalDate getLimitDate() {
+        return limitDate;
+    }
+    public boolean isIsemergency() {
+        return isemergency;
+    }
+    public void setId(int id) {
+    }
+    public void setCategory(int category) {
+        this.category = category;
+    }
+    public void setLimitAmount(int limitAmount) {
+        this.limitAmount = limitAmount;
+    }
+    public void setLimitDate(LocalDate limitDate) {
+        this.limitDate = limitDate;
+    }
+    public void setIsemergency(boolean isemergency) {
+        this.isemergency = isemergency;
+    }
+    @Override
+    public String toString() {
+        return "VirtualAccount [id=" + id + ", category=" + category + ", limitAmount=" + limitAmount + ", limitDate="
+                + limitDate + ", isemergency=" + isemergency + ", balance=" + balance + "]";
+    }
+    
 }
